fee2c964af87159af8f2cc5c761048b2d5521e80
originalpau <pauhua@kth.se>
2022-02-23 00:01:22 +0100

    Update shunt.ex

--- train/shunt.ex
+++ train/shunt.ex
@@ -14,8 +14,19 @@ defmodule Shunt do
   def find(xs, [y|ys], moves) do
     {hs, ts} = split(xs, y)
     found_moves = [{:one, length(ts)+1}, {:two, length(hs)}, {:one, (length(ts)+1)*-1}, {:two, (length(hs)*-1)}]
-    [_,_,_,_,{[x|new_xs],[],[]}] = move(found_moves, {xs,[],[]})
+    [_,_,_,_,{[_x|new_xs],[],[]}] = move(found_moves, {xs,[],[]})
     find(new_xs, ys, append(moves,found_moves))
   end
 
+  def few([],[]) do [] end
+  def few(xs, ys) do few(xs, ys, []) end
+
+  def few([], [], moves) do moves end
+  def few([e|xs], [e|ys], moves) do few(xs, ys, moves) end
+  def few(xs, [y|ys], moves) do
+    {hs, ts} = split(xs, y)
+    found_moves = [{:one, length(ts)+1}, {:two, length(hs)}, {:one, (length(ts)+1)*-1}, {:two, (length(hs)*-1)}]
+    [_,_,_,_,{[_x|new_xs],[],[]}] = move(found_moves, {xs,[],[]})
+    few(new_xs, ys, append(moves,found_moves))
+  end
 end
